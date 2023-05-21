e0c22c78aef6c2ab8ad42758433af1d13f643fc4
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-05-21 12:27:52 -0400

    form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import styles from '../page.module.css'
 
 const page = () => {
@@ -28,6 +29,29 @@ const page = () => {
                 </p>
             </form>
         </div>
+
+        <div>
+            <a
+            href="https://docs.netlify.com/forms/setup/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <h2>
+            Code used to make the above HTML form
+            </h2>
+            <p>Netlify Docs on forms</p>
+            <p>Make sure you have the html that's in the red retangle.</p>
+            </a>
+        </div>
+        <div>
+        <Image
+          src="/code.png"
+          alt="code from Netlify for forms"
+          width={840}
+          height={420}
+          priority
+        />
+      </div>
 
     </main>
   )
