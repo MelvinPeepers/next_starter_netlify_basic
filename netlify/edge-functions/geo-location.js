677449f49d41fe0a324812caa1e9ccd7a5c164e5
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-06-11 13:11:04 -0400

    trying out geo-location block

--- netlify/edge-functions/geo-location.js
+++ netlify/edge-functions/geo-location.js
@@ -1,18 +1,10 @@
 import { Context } from "https://edge.netlify.com";
 
-export default async (request: Request, context: Context) => {
-  const BLOCKED_COUNTRY_CODE = "GB";
-  const countryCode = context.geo?.country?.code || "US";
-  const countryName = context.geo?.country?.name || "United States of America";
-
-  if (countryCode === BLOCKED_COUNTRY_CODE) {
-    return new Response(`We're sorry, you can't access our content from ${countryName}!`, {
-      headers: { "content-type": "text/html" },
-      status: 451,
-    });
-  }
-
-  return new Response(`Hello there! You can freely access our content from ${countryName}!`, {
-    headers: { "content-type": "text/html" },
-  });
-};
+export default async (req: Request, { geo }: Context) => {
+    // Bypassing if the request is not from Portugal.
+    if (geo.country.code !== "US") {
+      return;
+    }
+  
+    return new Response("Hello USA!");
+  };
