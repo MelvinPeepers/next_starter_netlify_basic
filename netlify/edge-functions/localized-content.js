886913f2db4b86a0abb519fbaa6e4f75467b65de
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-06-13 12:19:07 -0400

    hello-world example ef

--- netlify/edge-functions/localized-content.js
+++ netlify/edge-functions/localized-content.js
@@ -1,18 +1,8 @@
 export default async (request, context) => {
-  const translations = {
-    UNKNOWN: "Hello!",
-    US: "Howdy y'all!",
-    GB: "How do you do?",
-    AU: "G'day, mate!",
-  };
-
-  const countryCode = context.geo?.country?.code;
-  const translation = translations[countryCode] || translations["UNKNOWN"];
-  const countryName = context.geo?.country?.name || "somewhere in the world";
-
-  return new Response(`Your personalized greeting for ${countryName} is: ${translation}`, {
-    headers: { "content-type": "text/html" },
+  return new Response("Hello, World!", {
+    headers: { "content-type": "text/plain" },
   });
 };
+
 
 
