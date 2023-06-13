33e5cedddc8a29384e84c44eabc31d1f2400e1a9
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-06-13 05:53:39 -0400

    new EF code in JS with Fetch

--- netlify/edge-functions/geo-location.js
+++ netlify/edge-functions/geo-location.js
@@ -1,4 +1,4 @@
-import { Context } from "https://edge.netlify.com";
+import { fetch } from "https://cdnjs.cloudflare.com/ajax/libs/node-fetch/2.6.1/index.js";
 
 export default async (request, context) => {
   // Here's what's available on context.geo
@@ -20,9 +20,17 @@ export default async (request, context) => {
   //   }
   // }
 
-  return new Response(JSON.stringify({
-    geo: context.geo,
-  }), {
-    headers: { "content-type": "application/json" },
+  const response = await fetch("https://edge.netlify.com", {
+    headers: { "Content-Type": "application/json" },
+    method: "POST",
+    body: JSON.stringify({
+      request: request,
+      context: context,
+    }),
   });
+
+  const responseBody = await response.json();
+
+  return responseBody;
 };
+
