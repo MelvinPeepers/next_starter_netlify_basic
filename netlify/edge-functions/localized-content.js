b7dcaece454035713028537beeeb9aab94e8bdbd
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-06-14 04:11:02 -0400

    changing EF

--- netlify/edge-functions/localized-content.js
+++ netlify/edge-functions/localized-content.js
@@ -1,5 +1,5 @@
 export default async (request, context) => {
-  return new Response("Hello, World!", {
+  return new Response("Hello, World!!", {
     headers: { "content-type": "text/plain" },
   });
 };
